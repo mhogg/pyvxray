45851353c470f7a692d1ad72a929b629fbf93e64
Michael Hogg <michael.christopher.hogg@gmail.com>
2013-09-27 22:52:08 +1000

    Updated pyvXRAY_plugin.py
    Added check of step list

--- pyvXRAY/pyvXRAY_plugin.py
+++ pyvXRAY/pyvXRAY_plugin.py
@@ -26,18 +26,18 @@ class PyvXRAY_plugin(AFXForm):
         self.bPartNameKw         = AFXStringKeyword(self.cmd, 'bPartName', True, 'PART-1-1')
         self.bSetNameKw          = AFXStringKeyword(self.cmd, 'bSetName', True, 'BONE')
         self.BMDfonameKw         = AFXStringKeyword(self.cmd, 'BMDfoname', True, 'SDV1')
-        self.showImplantKw       = AFXBoolKeyword(self.cmd, 'showImplant', AFXBoolKeyword.TRUE_FALSE, True, False)
+        self.showImplantKw       = AFXBoolKeyword(self.cmd,   'showImplant', AFXBoolKeyword.TRUE_FALSE, True, False)
         self.iPartNameKw         = AFXStringKeyword(self.cmd, 'iPartName', True, 'PART-1-1')
         self.iSetNameKw          = AFXStringKeyword(self.cmd, 'iSetName', True, 'IMPLANT')
-        self.iDensityKw          = AFXFloatKeyword(self.cmd, 'iDensity', True, 4500)
-        self.stepListKw          = AFXStringKeyword(self.cmd, 'stepList', True, '1, 2, 3, 4, 5')
+        self.iDensityKw          = AFXFloatKeyword(self.cmd,  'iDensity', True, 4500)
+        self.stepListKw          = AFXStringKeyword(self.cmd, 'stepList', True, '1')
         self.csysNameKw          = AFXStringKeyword(self.cmd, 'csysName', True, 'CSYS-1')
-        self.resGridKw           = AFXFloatKeyword(self.cmd, 'resGrid', True, 2)
+        self.resGridKw           = AFXFloatKeyword(self.cmd,  'resGrid', True, 2)
         self.imageNameBaseKw     = AFXStringKeyword(self.cmd, 'imageNameBase', True, 'vxray')
-        self.preferredXraySizeKw = AFXIntKeyword(self.cmd, 'preferredXraySize', True, 800)
+        self.preferredXraySizeKw = AFXIntKeyword(self.cmd,    'preferredXraySize', True, 800)
         self.imageFormatKw       = AFXStringKeyword(self.cmd, 'imageFormat', True, self.imageFormats[-1])
-        self.smoothKw            = AFXBoolKeyword(self.cmd, 'smooth', AFXBoolKeyword.TRUE_FALSE, True, True)
-        self.manualScalingKw     = AFXBoolKeyword(self.cmd, 'manualImageScaling', AFXBoolKeyword.TRUE_FALSE, True, False)
+        self.smoothKw            = AFXBoolKeyword(self.cmd,   'smooth', AFXBoolKeyword.TRUE_FALSE, True, True)
+        self.manualScalingKw     = AFXBoolKeyword(self.cmd,   'manualImageScaling', AFXBoolKeyword.TRUE_FALSE, True, False)
         self.showImplant         = None
 
     def getFirstDialog(self):
@@ -84,8 +84,18 @@ class PyvXRAY_plugin(AFXForm):
         minXraySize = 100
         if preferredXraySize < minXraySize:
             showAFXErrorDialog(self.getCurrentDialog(), 'Minimum virtual x-ray image size is %i pixels' % minXraySize)
+            return False    
+
+        # Check that values in stepList are valid. Also check that the values are in increasing order
+        try: stepList = stepList.split(',')
+        except: 
+            showAFXErrorDialog(self.getCurrentDialog(), 'Error in step list')
+            return False
+        diff = [True for i in range(len(stepList)-1) if stepList[i+1]-stepList[i]<0]
+        if len(diff)>0: 
+            showAFXErrorDialog(self.getCurrentDialog(), 'Step numbers in step list not in increasing order' )
             return False          
-        
+            
         # Check that all steps in step list exist and that density variable exists in all steps (last frame)
         # NOTE: Do this last because it is the most time consuming
         stepList  = eval(self.stepListKw.getValue())
@@ -107,8 +117,12 @@ class PyvXRAY_plugin(AFXForm):
 
 # Register the plug-in
 
-thisPath = os.path.abspath(__file__)
-thisDir  = os.path.dirname(thisPath)
+desc  = 'An ABAQUS plugin used to generate a time series of virtual x-rays from the output of a bone remodelling analysis.\n\n'                  + \
+        'The resulting images can be used to analyse the change in bone density over time in a number of regions of interest or "Gruen Zones", ' + \
+        'typically due to changes in loading following insertion of an orthopaedic implant. Associated tool BMDanalyse, available on PyPi, was ' + \ 
+        'created for this sole purpose.\n\nRequires an odb file to be open within the current viewport which has a fieldoutput representing '    + \
+        'bone density. Works by mapping the bone density fieldoutput onto a regular 3D grid and then projecting the values onto the three '      + \
+        'orthogonal planes. Currently only models with C3D4 or C3D10 elements are supported.'
 
 toolset = getAFXApp().getAFXMainWindow().getPluginToolset()
 toolset.registerGuiMenuButton(
@@ -120,6 +134,6 @@ toolset.registerGuiMenuButton(
     applicableModules=ALL,
     version=__version__,
     author='Michael Hogg',
-    description='An ABAQUS plugin used to generate a time series of virtual x-rays from the output of a bone remodelling analysis.\n\nThe resulting images can be used to analyse the change in bone density over time in a number of regions of interest or "Gruen Zones", typically due to changes in loading following insertion of an orthopaedic implant. Associated tool BMDanalyse, available on PyPi, was created for this sole purpose.\n\nRequires an odb file to be open within the current viewport which has a fieldoutput representing bone density. Works by mapping the bone density fieldoutput onto a regular 3D grid and then projecting the values onto the three orthogonal planes. Currently only models with C3D4 or C3D10 elements are supported.',
+    description=desc,
     helpUrl='https://code.google.com/p/pyvxray/'
 )
